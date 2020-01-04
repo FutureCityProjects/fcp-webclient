5b7526fba2d9da3e0b2faced11814cd8538e4013
Jakob Schumann <jakob.schumann@vrok.de>
2020-01-04 16:51:59 +0000

    upd: refactor #2

--- src/api/client.ts
+++ src/api/client.ts
@@ -8,41 +8,42 @@ export class FCPClient extends HydraClient {
       .then((res) => res.token)
   }
 
-  public refreshAuthToken = (token: string): Promise<string> => {
-    return this.get("/refresh_token", {}, token)
+  public refreshAuthToken = (): Promise<string> => {
+    return this.get("/refresh_token")
       .then((res) => res.token)
   }
 
-  public getUser = (id: number, token: string): Promise<IUser> => {
-    return this.get(`/users/${id}`, {}, token)
+  public getUser = (id: number): Promise<IUser> => {
+    return this.get(`/users/${id}`)
   }
 
-  public getUserByUsername = (username: string, token: string): Promise<IUser> => {
-    return this.getUsers(token, { username }).then((users) => {
+  public getUserByUsername = (username: string): Promise<IUser> => {
+    return this.getUsers({ username }).then((users) => {
       return users["hydra:member"].length ? users["hydra:member"][0] : null
     })
   }
 
-  public getUsers = (token: string, query: any = {}): Promise<IHydraCollection<IUser>> => {
-    return this.get("/users", query, token)
+  public getUsers = (query: any = {}): Promise<IHydraCollection<IUser>> => {
+    return this.get("/users", query)
   }
 
-  public getCurrentProcess = (token: string): Promise<IProcess> => {
-    return this.getProcesses(token).then((processes) => {
+  // @todo update when multi-processes are implemented
+  public getCurrentProcess = (): Promise<IProcess> => {
+    return this.getProcesses().then((processes) => {
       return processes["hydra:member"].length ? processes["hydra:member"][0] : null
     })
   }
 
-  public getProcesses = (token: string): Promise<IHydraCollection<IProcess>> => {
-    return this.get("/processes", {}, token)
+  public getProcesses = (): Promise<IHydraCollection<IProcess>> => {
+    return this.get("/processes")
   }
 
-  public createProcess = (process: IProcess, token: string): Promise<IProcess> => {
-    return this.post("/processes", process, token)
+  public createProcess = (process: IProcess): Promise<IProcess> => {
+    return this.post("/processes", process)
   }
 
-  public updateProcess = (process: IProcess, token: string): Promise<IProcess> => {
-    return this.put(process["@id"], process, token)
+  public updateProcess = (process: IProcess): Promise<IProcess> => {
+    return this.put(process["@id"], process)
   }
 }
 
